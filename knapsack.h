3f132263316c6343fe5d70174f90393fb3e18ffe
TJ Dokas <tjdokas@gmail.com>
2021-08-16 23:21:49 -0400

    modified:   knapsack.h

--- knapsack.h
+++ knapsack.h
@@ -6,6 +6,43 @@
 
 using namespace std;
 
+vector<size_t> mergesort(vector<size_t> items) {
+    if (items.size() == 1)
+    {
+        if (items.back() < items.front())
+        {
+            swap(items.front(), items.back());
+        }
+        return items;
+    }
+
+    size_t middle = items.size() / 2;
+
+    vector<size_t>::iterator middleIter(items.begin());
+    advance(middleIter, middle);
+
+    vector<size_t> first(items.begin(),middleIter);
+    vector<size_t> second(middleIter,items.end());
+
+    vector<size_t>::iterator fiter = first.begin();
+    vector<size_t>::iterator siter = second.begin();
+    items.clear();
+
+    while (fiter != first.end() && (siter != second.end()))
+    {
+        if (*fiter < *siter)
+        {
+            items.push_back(*fiter);
+            fiter++;
+        }
+        else {
+            items.push_back(*siter);
+            siter++;
+        }
+    }
+    return items;
+}
+
 class knapsack {
     double cap;
     vector<size_t> items;
